5dcd34ece23769e85fe795a9cb558bcbbc14770e
crinaldi <csrinaldi@gmail.com>
2015-11-07 10:54:50 -0300

    merge

---
+++ gen/com/google/gwt/lang/com_00046workingflows_00046js_00046jscore_00046JSCore__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046workingflows_00046js_00046jscore_00046JSCore__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
